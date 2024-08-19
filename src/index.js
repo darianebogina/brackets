5d0331c9047412a1aac700b946a3f9be5a71af3f
unknown <dasha.nebogina@mail.ru>
2024-08-19 18:51:28 +0300

    feat: add simple algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  
+    if (str.length % 2 == 0) {
+        for (let i = 0; i <= str.length; i++) {
+            str = str.replaceAll('()', '');
+        }
+    }
+    else {
+        return false;
+    }
+    return str;
 }
