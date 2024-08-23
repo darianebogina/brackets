915284f7f0ed025136fe103f24e3c42aef63692e
unknown <dasha.nebogina@mail.ru>
2024-08-23 13:05:20 +0300

    feat: add even check

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
-    if (str.length % 2 == 0) {
-        for (let i = 0; i <= str.length; i++) {
-            str = str.replaceAll('()', '');
-        }
-    }
-    else {
+    if (!(str.length % 2 == 0)) {
         return false;
     }
-    return str;
+    
 }
