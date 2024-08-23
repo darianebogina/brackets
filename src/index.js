88b7ad170563e953a20a4fff70ae20ccc7f64515
unknown <dasha.nebogina@mail.ru>
2024-08-23 13:14:49 +0300

    feat: add simple remove

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ module.exports = function check(str, bracketsConfig) {
     if (!(str.length % 2 == 0)) {
         return false;
     }
+    str_brackets = bracketsConfig.flat().join('');
+
+    for (let i = 0; i <= str.length; i++) {
+        str = str.replaceAll(str_brackets, '');
+        console.log(str);
+    }
     
+    if (str.length == 0) {
+        return true;
+    }
+    else { return false; }
 }
